8de4116c185b56125e97fcfeedcb5e9cb4cfa621
giovannidcp <giovanniwcosta@usp.br>
2019-08-07 15:36:48 -0300

    My First Server and Request

---
+++ HTTPModule/server.js
@@ -0,0 +1,21 @@
+const {createServer, request} = require("http");
+
+let requestStream =  request({
+	hostname: "eloquentjavascript.net",
+	path: "/20_node.html",
+	method: "GET",
+	headers: {Accept: "text/html"}
+}, response => {console.log("Server responded with code", response.statusCode)});
+
+requestStream.end();
+
+let server = createServer((request, response) => {
+	response.writeHead(200, {"Content-Type": "text/html"});
+	response.write(`
+		<h1>Hello!</h1>
+		<p>You asked for <code>${request.url}</code></p>`);
+	response.end();
+});
+
+server.listen(8000);
+console.log("Listening! (port 8000)");
